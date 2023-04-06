724d9fdd3c7242d2594d08f0a54ff27c0fdc21be
Mab99V <mabelperezgaribay@gmail.com>
2023-04-06 10:36:54 -0500

    componente characters

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,24 @@
 import image from './../img/rick.jpeg';
 import './../styles/App.css';
+import {useState} from "react"
+import Characters from './Characters';
 
 function App() {
-  const reqApi = () =>{
-    console.log("Clicking");
+ const [characters, setCharacters] = useState(null);
+
+  const reqApi = async () => {
+    const api = await fetch('https://rickandmortyapi.com/api/character')
+    const characterApi = await api.json();
+    console.log(characters);
+    setCharacters(characterApi.results);
+   
+    
   }
   return (
     <div className="App">
       <header className="App-header">
        <h1 className="title">Rick & Morty</h1>
+       <Characters/>
        <img 
        src={image} 
        alt="Rick & Morty" 
---
+++ src/components/Characters.js
@@ -0,0 +1,9 @@
+
+
+export default function Characters() {
+  return (
+    <div>
+    <h1>Desde characters</h1>
+    </div>
+  )
+}
